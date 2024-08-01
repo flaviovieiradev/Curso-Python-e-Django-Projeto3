82ea5a832737f851a4090cddaad5b3916dceafe5
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-31 23:19:15 -0300

    Término da aula Definindo os templates

--- fusion/settings.py
+++ fusion/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'fusion',
+        'NAME': 'fusion',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
